fbb357c96798342a6e9c661e994841325b017b30
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-04 17:12:32 -0500

    generation and validation methods for JWT

---
+++ MovieNightSheduler/Authorization/JwtUtils.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using MovieNightScheduler.Models;
+using MovieNightScheduler.Helpers;
+
+namespace MovieNightScheduler.Authorization
+{
+    public interface IJwtUtils
+    {
+        public string GenerateJwtToken(User user);
+        public int? ValidateJwtToken(string token);
+        public RefreshToken GenerateRefreshToken(string ipAddress);
+    }
+    public class JwtUtils : IJwtUtils
+    {
+        private AppDb Db;
+        private readonly AppSettings _appsettings;
+
+        public JwtUtils(AppDb db, IOptions<AppSettings> appSettings)
+        {
+            Db = db;
+            _appsettings = appSettings.Value;
+        }
+        public string GenerateJwtToken(User user)
+        {
+            //generate token with a valid time of 15 minutes
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appsettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+        public int? ValidateJwtToken(string token)
+        {
+            if (token == null) return null;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appsettings.Secret);
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    //set clocksew to 0 so tokens expire on time
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                var jwtToken = (JwtSecurityToken)validatedToken;
+                var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+                //return user id from token if validation is successful
+                return userId;
+                
+            }
+            catch { return null; }
+        }
+    }
+}

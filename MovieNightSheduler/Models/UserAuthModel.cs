971a5984e7ded5838cc31bfca416701331f1f5c3
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-04 16:05:06 -0500

    Models for user authorization

--- MovieNightSheduler/Models/UserAuthModel.cs
+++ MovieNightSheduler/Models/UserAuthModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 namespace MovieNightScheduler.Models
 {
     public class UserAuth
@@ -8,5 +9,48 @@ namespace MovieNightScheduler.Models
 
         [Required]
         public string Password { get; set; }
+    }
+    public class AuthResponse
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string JwtToken { get; set; }
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
+    
+    }
+
+    public class CreateRequest
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class ResetPasswordRequest
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class RevokeTokenRequest
+    {
+        public string Token { get; set; }
+    }
+    public class ValidateResetTokenRequest
+    {
+        [Required]
+        public string Token { get; set; }
     }
 }

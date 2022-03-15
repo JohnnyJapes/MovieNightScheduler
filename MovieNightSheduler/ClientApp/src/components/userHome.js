3ad07101b9fd79c5ce0de44a669f607787dc76fd
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-15 17:00:46 -0400

    encoding change

--- MovieNightSheduler/ClientApp/src/components/userHome.js
+++ MovieNightSheduler/ClientApp/src/components/userHome.js
@@ -1,15 +1,12 @@
-﻿import React, { Component } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
 export class UserHome extends Component {
     static displayName = UserHome.name;
     constructor(props) {
         super(props);
-        let temp = JSON.parse(localStorage.getItem('user'));
         this.state = {
-            id: temp.id,
-            username: temp.username,
-            token: temp.jwtToken
+            user: JSON.parse(localStorage.getItem('user'))
         };
         console.log(this.state.username)
     }
@@ -18,7 +15,7 @@ export class UserHome extends Component {
     render() {
         return (
             <div>
-                <h1>{this.state.username}'s Home</h1>
+                <h1>{this.state.user.username}'s Home</h1>
 
 
             </div>

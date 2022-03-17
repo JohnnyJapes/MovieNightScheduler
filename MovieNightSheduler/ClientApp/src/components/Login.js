db9aa96884247ba4929cf8b6f90cbf95ed1c06b6
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-17 16:03:11 -0400

    Login page redone to use formik +yup validation plus axios

--- MovieNightSheduler/ClientApp/src/components/Login.js
+++ MovieNightSheduler/ClientApp/src/components/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import auth from "../services/auth.service"
+import { LoginSchema } from "../schemas/login"
+import { Formik, Form, Field } from 'formik';
 
 export class Login extends Component{
     static displayName = Login.name;
@@ -8,17 +11,23 @@ export class Login extends Component{
         super(props);
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            showAlert: false,
+            alertMessage: ""
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCloseAlert = this.handleCloseAlert.bind(this);
 
     }
 
-    handleSubmit = async (event) => {
-        event.preventDefault();
+    handleSubmit = async (data) => {
+/*        event.preventDefault();
         console.log(event.target.username.value + " " + event.target.password.value);
-        this.login(event.target.username.value, event.target.password.value)
+        console.log(LoginSchema);
+        this.login(event.target.username.value, event.target.password.value)*/
+        console.log(data);
+        this.login(data.username, data.password)
     }
 
     handleChange = async (event) => {
@@ -29,41 +38,88 @@ export class Login extends Component{
             [name]: value
         });
     }
+    handleCloseAlert()  {
+        this.setState({ showAlert: false });
+    }
 
 
 
     render() {
+        const showAlert = this.state.showAlert
+        let alert;
+        if (showAlert) {
+            alert = <div id="alertPlaceholder" className="alert alert-danger alert-dismissible fade show" role="alert">
+                {this.state.alertMessage}
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={this.handleCloseAlert}></button>
+                    </div>;
+        }
         return (
+
             <div>
-                <form onSubmit={this.handleSubmit}>
+                {alert}
 
-                    <div class="row mb-3 justify-content-center">
+                <Formik
+                    initialValues={{
+                        username: '',
+                        password: ''
+                    }}
+                    validationSchema={LoginSchema}
+                    onSubmit={this.handleSubmit}
+                >
+                    {({ errors, touched }) => (
+                        <Form>
 
-                        <div class="col-lg-6">
-                            <label for="username" class="form-label">Username</label>
-                            <input type="text" required class="form-control"
-                                id="username" aria-describedby="userHelp" name="username"
-                                onChange={this.handleChange} value={this.state.username} ></input>
-                        </div>
-                        <div class="col-lg-6">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" id="password" required class="form-control"
-                                aria-describedby="pwHelp" onChange={this.handleChange} value={this.state.password} ></input>
-                        </div>
+                            <div className="row mb-3 justify-content-center">
+
+                                <div className="col-lg-6">
+                                    <label htmlFor="username" className="form-label">Username</label>
+                                    <Field type="text" required className="form-control"
+                                        id="username" aria-describedby="userHelp" name="username" />
+                                    {/* onChange={this.handleChange} value={this.state.username} ></input>*/}
+                                    {errors.username && touched.username ? (<div className="text-danger">{errors.username}</div>) : null}
+
+                                </div>
+                                <div className="col-lg-6">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <Field type="password" name="password" id="password" required className="form-control" />
+                                    {/* aria-describedby="pwHelp" onChange={this.handleChange} value={this.state.password} ></input>*/}
+                                    {errors.password && touched.password ? (<div className="text-danger">{errors.password}</div>) : null}
+                                </div>
 
 
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-lg-3">
-                            <button class="btn btn-success" type="submit">Login</button>
-                        </div>
-                    </div>
-                </form>
+                            </div>
+                            <div className="row mb-3">
+                                <div className="col-lg-3">
+                                    <button className="btn btn-success" type="submit">Login</button>
+                                </div>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         );
     }
     async login(username, password) {
         try {
+            let response = await auth.login(username, password);
+            console.log(response);
+            this.setState({ username: "" });
+            this.setState({ password: "" });
+        }
+        catch(err) {
+            console.log(err.response.data.message);
+            this.setState({
+                showAlert: true,
+                alertMessage: "Invalid Username or Password"
+            });
+
+        }
+
+
+
+
+
+        /*try {
             let response = await fetch("api/User/authenticate", {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
@@ -90,7 +146,7 @@ export class Login extends Component{
         }
         catch (err) {
             console.log(err);
-        }
+        }*/
 
     }
 }

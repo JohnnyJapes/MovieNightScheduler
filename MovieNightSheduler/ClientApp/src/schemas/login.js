e68846ab1891892c6d5618a38b6d65e5fbd2a9db
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-17 16:01:43 -0400

    schemas for form validation

---
+++ MovieNightSheduler/ClientApp/src/schemas/login.js
@@ -0,0 +1,14 @@
+﻿import * as Yup from "yup";
+
+export const LoginSchema = Yup.object().shape({
+    username: Yup.string()
+        .min(2, "Username must be over 2 characters long")
+        .max(50, "Username can not be over 50 characters")
+        .matches(/^[A-Za-z0-9]+$/, { message: "valid characters: letters, numbers", excludeEmptyString: true })
+        .required("Required"),
+    password: Yup.string()
+        .min(2, "Password must be at least 2 characters long")
+        .max(60, "Passwords are not over 60 characters")
+        .matches(/[A-Za-z0-9!#$%&]+/, { message:"A-Za-z0-9!#$%&", excludeEmptyString: true })
+        .required("Required")
+});
---
+++ MovieNightSheduler/ClientApp/src/schemas/register.js
@@ -0,0 +1,14 @@
+﻿import * as Yup from "yup";
+
+const RegisterSchema = Yup.object().shape({
+    username: Yup.string()
+        .min(2, "Username must be over 2 characters long")
+        .max(50, "Username can not be over 50 characters")
+        .matches(/^[A-Za-z0-9]+$/, {message: "valid characters: letters, numbers", excludeEmptyString: true })
+        .required("Required"),
+    password: Yup.string()
+        .min(2, "Password must between 2-60 characters")
+        .max(60, "Password must between 2-60 characters")
+        .matches(/^[A-Za-z0-9!#$%&]+$/, { message: "use valid characters", excludeEmptyString: true })
+        .required("required")
+});

3f57e0f58f5d88b67f86b77f5f326652c70cd936
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-22 12:19:08 -0400

    NavMenu now using ReactHooks

--- MovieNightSheduler/ClientApp/src/components/NavMenu.js
+++ MovieNightSheduler/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+
+
+export function NavMenu(props) {
+    const displayName = NavMenu.name;
+    const [collapsed, setCollapsed] = useState(true);
+
+    return (
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                <Container>
+                    <NavbarBrand tag={Link} to="/">MovieNightSheduler</NavbarBrand>
+                    <NavbarToggler onClick={() => setCollapsed(!collapsed)} className="mr-2" />
+                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+                        <ul className="navbar-nav flex-grow">
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/register">Register</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/Login">Login</NavLink>
+                            </NavItem>
+
+                        </ul>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+
+}
+/*
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -21,7 +61,8 @@ export class NavMenu extends Component {
     });
   }
 
-  render () {
+    render() {
+      
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
@@ -39,6 +80,7 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
+                
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/register">Register</NavLink>
                 </NavItem>
@@ -54,3 +96,4 @@ export class NavMenu extends Component {
     );
   }
 }
+*/

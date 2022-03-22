ca2cf0f0177dffe3cc117b9793554c3e51ec7c67
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-22 12:23:06 -0400

    register redone using ReactHooks

--- MovieNightSheduler/ClientApp/src/components/Register.js
+++ MovieNightSheduler/ClientApp/src/components/Register.js
@@ -1,10 +1,111 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import auth from "../services/auth.service";
+import { Formik, Form, Field } from 'formik';
+import { RegisterSchema } from "../schemas/register"
 
 
-export class Register extends Component {
+
+export function Register(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState({ username: "", password: "" });
+
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+    const [alertType, setAlertType] = useState("alert-danger");
+
+    function handleSubmit(data) {
+        register(data.username, data.password);
+    }
+    function handleChange(event, user) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        setUser({
+            [name]: value
+        });
+    }
+    async function register(username, password) {
+        try {
+            let response = await auth.register(username, password);
+            console.log(response);
+            setAlertType("alert-success");
+            setAlertMessage("Account Created Successfully")
+            setShowAlert(true);
+            setUser({ username: "", password: "" });
+        }
+        catch (err) {
+            console.log(err.response.data);
+            
+            setAlertType("alert-danger");
+            const regex = new RegExp('Duplicate')
+            if (regex.test(err.response.data))
+                setAlertMessage("Username already in use");
+            else
+                setAlertMessage("Error Occurred")
+            setShowAlert(true);
+
+        }
+    }
+
+    let alert;
+    if (showAlert) {
+        alert = <div id="alertPlaceholder" className={`alert ${alertType} alert-dismissible fade show`} role="alert">
+            {alertMessage}
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=> setShowAlert(false)}></button>
+        </div>;
+    }
+    return (
+        <div>
+            <h3>Register</h3>
+            {alert}
+
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: ''
+                }}
+                validationSchema={RegisterSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+
+                        <div className="row mb-3 justify-content-center">
+
+                            <div className="col-lg-6">
+                                <label htmlFor="username" className="form-label">Username</label>
+                                <Field type="text" required className="form-control"
+                                    id="username" aria-describedby="userHelp" name="username" />
+                                {/* onChange={this.handleChange} value={this.state.username} ></input>*/}
+                                {errors.username && touched.username ? (<div className="text-danger">{errors.username}</div>) : null}
+
+                            </div>
+                            <div className="col-lg-6">
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <Field type="password" name="password" id="password" required className="form-control" />
+                                {/* aria-describedby="pwHelp" onChange={this.handleChange} value={this.state.password} ></input>*/}
+                                {errors.password && touched.password ? (<div className="text-danger">{errors.password}</div>) : null}
+                            </div>
+
+
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col-lg-3">
+                                <button className="btn btn-success" type="submit">Register</button>
+                            </div>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+
+}
+
+
+/*export class Register extends Component {
     static displayName = Register.name;
-
-     
 
     constructor(props) {
         super(props);
@@ -39,25 +140,25 @@ export class Register extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
 
-                <div class="row mb-3 justify-content-center">
+                    <div className="row mb-3 justify-content-center">
 
-                    <div class="col-lg-6">
-                        <label for="username" class="form-label">Username</label>
-                            <input type="text" required class="form-control"
+                        <div className="col-lg-6">
+                            <label htmlFor="username" className="form-label">Username</label>
+                            <input type="text" required className="form-control"
                                 id="username" aria-describedby="userHelp" name="username"
                                 onChange={this.handleChange} value={this.state.username} ></input>
                     </div>
-                        <div class="col-lg-6">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" id="password" required class="form-control"
+                        <div className="col-lg-6">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" name="password" id="password" required className="form-control"
                                 aria-describedby="pwHelp" onChange={this.handleChange} value={this.state.password} ></input>
                         </div>
 
 
                 </div>
-                            <div class="row mb-3">
-                                <div class="col-lg-3">
-                                    <button class="btn btn-success" type="submit">Register</button>
+                    <div className="row mb-3">
+                        <div className="col-lg-3">
+                            <button className="btn btn-success" type="submit">Register</button>
                                 </div>
                             </div>
             </form>
@@ -95,3 +196,4 @@ export class Register extends Component {
         
     }
 }
+*/

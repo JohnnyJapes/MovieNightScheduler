69ef6ce49883cbadad6b9e90ea6d41de6d87e94c
JohnnyJapes <lhanrahan2@gmail.com>
2022-03-24 17:38:59 -0400

    small list component for groups on user page

--- MovieNightSheduler/ClientApp/src/components/userHome.js
+++ MovieNightSheduler/ClientApp/src/components/userHome.js
@@ -1,20 +1,52 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import axios from "axios";
 import auth from "../services/auth.service";
+import api from "../services/api";
+import userService from "../services/user.service";
 
 
 export function UserHome(props) {
 
     const [user, setUser] = useState(auth.getCurrentUser())
+    const [user2, setUser2] = useState();
+    const [isBusy, setBusy] = useState(true);
+    let response;
+    useEffect(() => {
+        response = userService.getUserInfo(user.id)
+            .then(response => {
+                console.log(response);
+                console.log(JSON.stringify(response.data));
+                setUser2(response.data);
+                setBusy(false);
+            });
+
+    }, [user])
+
+    if (!isBusy) {
+
+
+        return (
+            <div>
+                <h1>{user2.username}'s Home</h1>
+                <h3>Currently Part of Groups:</h3>
+                <List groups={user2.groups} />
+
+            </div>
+        )
+    }
     return (
-        <div>
-            <h1>{user.username}'s Home</h1>
-
-
-        </div>
+        <div> </div>
 
 
     )
+}
+function List(props) {
+    const groups = props.groups;
+    const groupList = groups.map((group) => {
+        console.log(group.name);
+        return <li key={group.id}>{group.name}</li>;
+    })
+    return (<ul>{groupList}</ul>)
 }
 
 

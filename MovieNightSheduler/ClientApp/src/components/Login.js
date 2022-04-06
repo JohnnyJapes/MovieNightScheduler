56950102884d012eeeb297761c3e2d8d1826aadf
JohnnyJapes <lhanrahan2@gmail.com>
2022-04-05 21:25:05 -0400

    some redirects and use of props

--- MovieNightSheduler/ClientApp/src/components/Login.js
+++ MovieNightSheduler/ClientApp/src/components/Login.js
@@ -3,12 +3,13 @@ import axios from "axios";
 import auth from "../services/auth.service"
 import { LoginSchema } from "../schemas/login"
 import { Formik, Form, Field } from 'formik';
+import { useNavigate, Redirect } from "react-router-dom";
 
 export function Login(props) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [user, setUser] = useState({ username: "", password: "" });
-
+    let navigate = useNavigate();
     const [showAlert, setShowAlert] = useState(false);
     const [alertMessage, setAlertMessage] = useState("");
     const [alertType, setAlertType] = useState("alert-danger");
@@ -20,10 +21,12 @@ export function Login(props) {
         try {
             let response = await auth.login(username, password);
             console.log(response);
+            props.setUser(auth.getCurrentUser());
             setAlertType("alert-success");
             setAlertMessage("Login Successful")
             setShowAlert(true);
             setUser({ username: "", password: "" });
+            navigate("/home")
         }
         catch (err) {
             console.log(err.response.data.message);
@@ -112,127 +115,127 @@ export function Login(props) {
         console.log(event.target.username.value + " " + event.target.password.value);
         console.log(LoginSchema);
         this.login(event.target.username.value, event.target.password.value)*//*
-        console.log(data);
-        this.login(data.username, data.password)
+console.log(data);
+this.login(data.username, data.password)
+}
+
+handleChange = async (event) => {
+const target = event.target;
+const value = target.value;
+const name = target.name;
+this.setState({
+[name]: value
+});
+}
+handleCloseAlert()  {
+this.setState({ showAlert: false });
+}
+
+
+
+render() {
+const showAlert = this.state.showAlert
+let alert;
+if (showAlert) {
+alert = <div id="alertPlaceholder" className="alert alert-danger alert-dismissible fade show" role="alert">
+{this.state.alertMessage}
+<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={this.handleCloseAlert}></button>
+    </div>;
+}
+return (
+
+<div>
+{alert}
+
+<Formik
+    initialValues={{
+        username: '',
+        password: ''
+    }}
+    validationSchema={LoginSchema}
+    onSubmit={this.handleSubmit}
+>
+    {({ errors, touched }) => (
+        <Form>
+
+            <div className="row mb-3 justify-content-center">
+
+                <div className="col-lg-6">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <Field type="text" required className="form-control"
+                        id="username" aria-describedby="userHelp" name="username" />
+                    {*//* onChange={this.handleChange} value={this.state.username} ></input>*//*}
+{errors.username && touched.username ? (<div className="text-danger">{errors.username}</div>) : null}
+
+</div>
+<div className="col-lg-6">
+<label htmlFor="password" className="form-label">Password</label>
+<Field type="password" name="password" id="password" required className="form-control" />
+{*//* aria-describedby="pwHelp" onChange={this.handleChange} value={this.state.password} ></input>*//*}
+{errors.password && touched.password ? (<div className="text-danger">{errors.password}</div>) : null}
+</div>
+
+
+</div>
+<div className="row mb-3">
+<div className="col-lg-3">
+<button className="btn btn-success" type="submit">Login</button>
+</div>
+</div>
+</Form>
+)}
+</Formik>
+</div>
+);
+}
+async login(username, password) {
+try {
+let response = await auth.login(username, password);
+console.log(response);
+this.setState({ username: "" });
+this.setState({ password: "" });
+}
+catch(err) {
+console.log(err.response.data.message);
+this.setState({
+showAlert: true,
+alertMessage: "Invalid Username or Password"
+});
+
+}
+
+
+
+
+
+*//*try {
+    let response = await fetch("api/User/authenticate", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            Username: username,
+            Password: password
+        })
+    });
+    console.log(JSON.stringify({
+        Username: username,
+        password: password
+    }));
+    let responseText = await response.json();
+    console.log(responseText);
+    if (response.status === 200) {
+        this.setState({ username: "" });
+        this.setState({ password: "" });
+        localStorage.setItem("user", JSON.stringify(responseText));
+        ;
+    } else {
+        alert("Username must be unique");
+        console.log("error");
     }
+}
+catch (err) {
+    console.log(err);
+}*//*
 
-    handleChange = async (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-            [name]: value
-        });
-    }
-    handleCloseAlert()  {
-        this.setState({ showAlert: false });
-    }
-
-
-
-    render() {
-        const showAlert = this.state.showAlert
-        let alert;
-        if (showAlert) {
-            alert = <div id="alertPlaceholder" className="alert alert-danger alert-dismissible fade show" role="alert">
-                {this.state.alertMessage}
-                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={this.handleCloseAlert}></button>
-                    </div>;
-        }
-        return (
-
-            <div>
-                {alert}
-
-                <Formik
-                    initialValues={{
-                        username: '',
-                        password: ''
-                    }}
-                    validationSchema={LoginSchema}
-                    onSubmit={this.handleSubmit}
-                >
-                    {({ errors, touched }) => (
-                        <Form>
-
-                            <div className="row mb-3 justify-content-center">
-
-                                <div className="col-lg-6">
-                                    <label htmlFor="username" className="form-label">Username</label>
-                                    <Field type="text" required className="form-control"
-                                        id="username" aria-describedby="userHelp" name="username" />
-                                    {*//* onChange={this.handleChange} value={this.state.username} ></input>*//*}
-                                    {errors.username && touched.username ? (<div className="text-danger">{errors.username}</div>) : null}
-
-                                </div>
-                                <div className="col-lg-6">
-                                    <label htmlFor="password" className="form-label">Password</label>
-                                    <Field type="password" name="password" id="password" required className="form-control" />
-                                    {*//* aria-describedby="pwHelp" onChange={this.handleChange} value={this.state.password} ></input>*//*}
-                                    {errors.password && touched.password ? (<div className="text-danger">{errors.password}</div>) : null}
-                                </div>
-
-
-                            </div>
-                            <div className="row mb-3">
-                                <div className="col-lg-3">
-                                    <button className="btn btn-success" type="submit">Login</button>
-                                </div>
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-        );
-    }
-    async login(username, password) {
-        try {
-            let response = await auth.login(username, password);
-            console.log(response);
-            this.setState({ username: "" });
-            this.setState({ password: "" });
-        }
-        catch(err) {
-            console.log(err.response.data.message);
-            this.setState({
-                showAlert: true,
-                alertMessage: "Invalid Username or Password"
-            });
-
-        }
-
-
-
-
-
-        *//*try {
-            let response = await fetch("api/User/authenticate", {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    Username: username,
-                    Password: password
-                })
-            });
-            console.log(JSON.stringify({
-                Username: username,
-                password: password
-            }));
-            let responseText = await response.json();
-            console.log(responseText);
-            if (response.status === 200) {
-                this.setState({ username: "" });
-                this.setState({ password: "" });
-                localStorage.setItem("user", JSON.stringify(responseText));
-                ;
-            } else {
-                alert("Username must be unique");
-                console.log("error");
-            }
-        }
-        catch (err) {
-            console.log(err);
-        }*//*
-
-    }
+}
 }*/
--- MovieNightSheduler/ClientApp/src/components/userHome.js
+++ MovieNightSheduler/ClientApp/src/components/userHome.js
@@ -3,23 +3,42 @@ import axios from "axios";
 import auth from "../services/auth.service";
 import api from "../services/api";
 import userService from "../services/user.service";
-
+import { useNavigate, Redirect } from "react-router-dom";
 
 export function UserHome(props) {
 
     const [user, setUser] = useState(auth.getCurrentUser())
     const [user2, setUser2] = useState();
     const [isBusy, setBusy] = useState(true);
+    const [viewings, setViewings] = useState();
+    let navigate = useNavigate();
     let response;
     useEffect(() => {
-        response = userService.getUserInfo(user.id)
-            .then(response => {
-                console.log(response);
-                console.log(JSON.stringify(response.data));
-                setUser2(response.data);
-                setBusy(false);
-            });
+        let loggedIn = true;
+        if (props.user == null) { navigate("/"); return; }
+        console.log(props.user);
+        const getUser = async () => {
 
+            response = await userService.getUserInfo(user.id)
+            console.log(response);
+            console.log(JSON.stringify(response.data));
+            setUser2(response.data);
+            if (loggedIn)
+                setBusy(false);
+        }
+        getUser()
+            // response = userService.getUserInfo(user.id)
+            //     .then(response => {
+            //         console.log(response);
+            //         console.log(JSON.stringify(response.data));
+            //         setUser2(response.data);
+            //         setBusy(false);
+            //     })
+            .catch((error) => {
+                console.error(error);
+                navigate("/");
+            });
+        return () => loggedIn = false;
     }, [user])
 
     if (!isBusy) {
@@ -30,8 +49,11 @@ export function UserHome(props) {
                 <h1>{user2.username}'s Home</h1>
                 <h3>Currently Part of Groups:</h3>
                 <List groups={user2.groups} />
-
+                <h3>Upcoming Viewings:</h3>
+                <List groups={user2.groups} />
+                <h5>{props.user.id}</h5>
             </div>
+
         )
     }
     return (
@@ -48,6 +70,15 @@ function List(props) {
     })
     return (<ul>{groupList}</ul>)
 }
+// function ListViewings(props) {
+//     const viewings = props.groups.viewings;
+//     const groupList = groups.map((group) => {
+//         console.log(group.name);
+//         return <li key={group.id}>{group.name}</li>;
+//     })
+//     return (<ul>{groupList}</ul>)
+// }
+
 
 
 /*export class UserHome extends Component {
